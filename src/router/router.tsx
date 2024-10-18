8cf197a27b50f4d82f654fb28090cddbadf4692c
Hassan Shahid <hassanshahid234@gmail.com>
2024-10-18 12:05:20 +0500

    router.tsx updated

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Home from "../pages/Home";
 import Login from "../pages/Login";
 import Map from "../pages/Map";
@@ -8,18 +8,26 @@ import ProtectedRoute from "./ProtectedRoute";
 import NotFound from "@/pages/NotFound";
 
 const Router = () => {
+  const isAuthenticated = () => {
+    const user = localStorage.getItem("user");
+    return user ? true : false;
+  };
+
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Login />} />
-          <Route path="/map" element={<Map />} />
+        <Route
+          path="/"
+          element={isAuthenticated() ? <Navigate to="/home" /> : <Login />}
+        />
+        <Route path="/map" element={<Map />} />
 
         <Route element={<ProtectedRoute />}>
           <Route path="/home/*" element={<Home />} />
           <Route path="/excelListing" element={<ExcelListing />} />
           <Route path="/displayExcelData/:id" element={<DisplayExcelData />} />
         </Route>
-        
+
         <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
